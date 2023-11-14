45f54821a5289e8715265e1618c1fd627b8ae17e
sandereddy1 <sande1@toromail.csudh.edu>
2023-11-14 23:39:22 +0000

    adding a python program

---
+++ sande11.py
@@ -0,0 +1,9 @@
+#Slesha Reddy Ande
+#CSC321
+#lab 11
+number = float(input("Enter a number: "))
+result = number ** 2
+professor_saying = "My professor is amazing!"
+
+print(f"Result: {result}, Saying: {professor_saying}")
+
